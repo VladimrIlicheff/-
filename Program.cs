977dbb9943e5423b9ac4931c3fb0bb5f21bdac45
Vladimir <user@MAK.local>
2022-12-04 20:17:13 +0700

    Добавлен цикл, в нем проходим по массиву и сравниваем длинну элементов массива с заданным числом 3

--- Program.cs
+++ Program.cs
@@ -2,3 +2,12 @@
 string str = new string (Console.ReadLine());
 string[] array = str.Split(new char[] {','});
 int quant = 0;
+for (int i=0; i<array.Length; i++)
+{
+    int num = Convert.ToInt32(array[i].Length);
+    if (num<=3) 
+    {
+        Console.Write(array[i]+ "; " );
+        quant++; 
+    }
+}
